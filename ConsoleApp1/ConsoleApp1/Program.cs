fc1cb3fa67f8748e5ba9108d730a41d39c0b11b6
Oskar Atamanenko <s32686@pjwstk.edu.pl>
2026-03-10 16:50:41 +0100

    Handle invalid user input name

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Write("Enter your name: ");
 string name = Console.ReadLine();
-Console.WriteLine($"Hello, {name}!");
+
+if (name != "" && !int.TryParse(name, out int nameInt))
+{
+    Console.WriteLine($"Hello, {name}!");
+}
+else
+{
+    Console.WriteLine("Invalid input. Please enter a valid name.");
+}
