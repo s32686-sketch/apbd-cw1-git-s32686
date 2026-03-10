91af23db66064c3cd05bcd25f7c55bcbae5e7ce7
Oskar Atamanenko <s32686@pjwstk.edu.pl>
2026-03-10 16:46:51 +0100

    Greeting user with input name

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}!");
